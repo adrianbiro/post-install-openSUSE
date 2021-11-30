5531a921b534b6cf2c888b8301314edb5b036005
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2021-11-30 11:30:59 +0100

    Add files via upload

---
+++ snapinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo zypper refresh
+# install snaps
+sudo snap install signal-desktop
+sudo snap install authy
+sudo snap install bitwarden
+echo "#############################"
+echo "########## Restart ##########"
+echo "#############################"
+
+
