0b065d8d4d2a51722ea0c5cce0d408b0a8974d74
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2021-11-30 12:22:57 +0100

    Update gitstuff.sh

--- gitstuff.sh
+++ gitstuff.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "#############    Security auditing tool   #######################"
+git clone https://github.com/CISOfy/lynis
 echo "#############   Adding templates for libreoffice   ##############"
 git clone https://github.com/adrianbiro/documents_templates
 cd documents_templates
