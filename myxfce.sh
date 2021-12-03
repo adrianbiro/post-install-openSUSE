4a35a0c75aa54302be63c7fcadb86cdced4e2377
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2021-12-03 14:54:36 +0100

    Update myxfce.sh

--- myxfce.sh
+++ myxfce.sh
@@ -2,12 +2,11 @@
 # custom shortcuts
 cp xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 # custom panel
-# TODO over
 cp xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+# sudo tar -xzvf panel_on_side.tar.gz -C ~/.config/xfce4/xfconf/xfce-perchannel-xml
 # Changing timezone to Bratislava. To find another use $ "timedatectl list-timezones |less" and search with "/" name_of_zone and quit with "q"
 sudo timedatectl set-timezone Europe/Bratislava
 #sudo timedatectl set-timezone Europe/Prague
 #sudo timedatectl set-timezone Europe/Vienna
 # Adding keyboard layouts to see another use $ "cat /usr/share/X11/xkb/rules/xorg.lst | less" fr for French, es for Spanish
-# TODO over
 setxkbmap -option grp:switch,grp:win_space_toggle us,sk
