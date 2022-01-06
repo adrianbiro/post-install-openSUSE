222c17799ec602cfafcf68138ba7c5b31be163f5
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2022-01-06 11:55:11 +0100

    Update gitstuff.sh

--- gitstuff.sh
+++ gitstuff.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "#############    Security auditing tool   #######################"
+# Security auditing tool   
 git clone https://github.com/CISOfy/lynis
-echo "#############   Adding templates for libreoffice   ##############"
+# Adding templates for libreoffice   
 git clone https://github.com/adrianbiro/documents_templates
 cd documents_templates
 sudo tar -xzvf Templates.tar.gz -C /home/adrian
+# custom panel profiles
+#git clone https://github.com/adrianbiro/xfce-panel-profiles
