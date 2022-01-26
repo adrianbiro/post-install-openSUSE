19ccced37b1ea4adcbf2953e658d3a3760f2fc58
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2022-01-26 14:38:30 +0100

    Update installzypper.sh

--- installzypper.sh
+++ installzypper.sh
@@ -5,7 +5,7 @@ sudo zypper install -y chromium
 sudo zypper install -y torbrowser-launcher
 sudo zypper install -y htop 
 sudo zypper install -y neofetch
-sudo zypper install -y dmenu
+sudo zypper install -y geany geany-plugins
 sudo zypper install -y xfce4-clipman-plugin 
 sudo zypper install -y xkill
 sudo zypper install -y redshift redshift-gtk bumblebee-status-module-redshift
