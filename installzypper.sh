7b1f233d6dd93bcd315dcad9c59c8233da7d247c
Adrián Bíro <89740062+adrianbiro@users.noreply.github.com>
2022-02-13 13:00:35 +0100

    Update installzypper.sh

--- installzypper.sh
+++ installzypper.sh
@@ -11,5 +11,6 @@ sudo zypper install -y xkill
 sudo zypper install -y redshift redshift-gtk bumblebee-status-module-redshift
 sudo zypper install -y tmux
 sudo zypper install -y nnn
+sudo zypper install -y entr
 sudo zypper install -y tesseract-ocr-traineddata-czech tesseract-ocr tesseract-ocr-traineddata-english tesseract-ocr-traineddata-german
 sudo zypper install -t pattern devel_basis
