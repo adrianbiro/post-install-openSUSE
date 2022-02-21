25aede43a3a565b1df2aa5ff26fdfcae8ccd25cc
Adrián Bíro <biroadrian@protonmail.com>
2022-02-21 22:26:52 +0100

    symlinks

--- installzypper.sh
+++ installzypper.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo zypper install -y git
 sudo zypper install -y meld
-sudo zypper install -y chromium 
+sudo zypper install -y chromium
 sudo zypper install -y torbrowser-launcher
-sudo zypper install -y htop 
+sudo zypper install -y htop
 sudo zypper install -y neofetch
 sudo zypper install -y geany geany-plugins
-sudo zypper install -y xfce4-clipman-plugin 
+sudo zypper install -y xfce4-clipman-plugin
 sudo zypper install -y xkill
 sudo zypper install -y redshift redshift-gtk bumblebee-status-module-redshift
 sudo zypper install -y tmux
-sudo zypper install -y nnn
+#sudo zypper install -y nnn
 sudo zypper install -y entr
 sudo zypper install -y tesseract-ocr-traineddata-czech tesseract-ocr tesseract-ocr-traineddata-english tesseract-ocr-traineddata-german
 sudo zypper install -t pattern devel_basis
---
+++ symlinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#dot files except .git and parent dir ..
+ln -s ~/bin/gits/.files/.*[^git.] ~/
+#vim
+ln -s ~/bin/gits/my_templates/ ~/.vim/
+ln -s ~/bin/gits/.files/pomoc.md ~/.vim/
