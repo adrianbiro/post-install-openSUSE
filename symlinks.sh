78d6a9b42bb9a2cea276c00d06c36a664b53d5ed
Adrián Bíro <biroadrian@protonmail.com>
2022-03-02 15:26:06 +0100

    ln -s

--- symlinks.sh
+++ symlinks.sh
@@ -2,7 +2,8 @@
 
 
 #dot files except .git and parent dir ..
-ln -s ~/bin/gits/.files/.*[^git.] ~/
+ln -s ~/bin/gits/dot_files/.*[^git.] ~/
 #vim
 ln -s ~/bin/gits/my_templates/ ~/.vim/
-ln -s ~/bin/gits/.files/pomoc.md ~/.vim/
+ln -s ~/bin/gits/dot_files/pomoc.md ~/.vim/
+ln -s ~/bin/gits/m_bin/*[^.git.] ~/bin
