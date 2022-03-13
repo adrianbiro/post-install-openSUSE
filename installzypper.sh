3d206716533a4352d87bfbe48573e39a973f3891
Adrián Bíro <biroadrian@protonmail.com>
2022-03-13 13:13:01 +0100

    zypp

--- installzypper.sh
+++ installzypper.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-sudo zypper install -y git
-sudo zypper install -y meld
-sudo zypper install -y chromium
-sudo zypper install -y torbrowser-launcher
-sudo zypper install -y htop
-sudo zypper install -y neofetch
-sudo zypper install -y geany geany-plugins
-sudo zypper install -y xfce4-clipman-plugin
-sudo zypper install -y xkill
-sudo zypper install -y redshift redshift-gtk \
-  bumblebee-status-module-redshift
-sudo zypper install -y tmux
-#sudo zypper install -y nnn
-sudo zypper install -y entr
-sudo zypper install -y ncdu
-sudo zypper install -y bc
-sudo zypper install -y perl-doc
-sudo zypper install -y exfatprogs fuse-exfat
-sudo zypper install -y tesseract-ocr-traineddata-czech \
-  tesseract-ocr tesseract-ocr-traineddata-english \
-  tesseract-ocr-traineddata-german
+sudo zypper --non-interactive install -y \
+git \
+meld \
+chromium \
+torbrowser-launcher \
+htop \
+neofetch \
+geany \
+geany-plugins \
+xfce4-clipman-plugin \
+xkill \
+redshift \
+redshift-gtk \
+bumblebee-status-module-redshift \
+tmux \
+entr \
+ncdu \
+bc \
+perl-doc \
+exfatprogs \
+fuse-exfat \
+tesseract-ocr-traineddata-czech \
+tesseract-ocr \
+tesseract-ocr-traineddata-english \
+tesseract-ocr-traineddata-german
+
+
 sudo zypper install -t pattern devel_basis
