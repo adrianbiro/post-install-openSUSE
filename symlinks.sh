426c474c8cca8402c385fe4861875b6513fc5c48
Adrián Bíro <biroadrian@protonmain.com>
2022-04-21 12:03:06 +0200

    small fixes

--- symlinks.sh
+++ symlinks.sh
@@ -2,8 +2,8 @@
 
 
 #dot files except .git and parent dir ..
-ln -s ~/bin/gits/dot_files/.*[^git.] ~/
+ln -s ~gits/dot_files/.*[^git.] ~/
 #vim
-ln -s ~/bin/gits/my_templates/ ~/.vim/
-ln -s ~/bin/gits/dot_files/pomoc.md ~/.vim/
-ln -s ~/bin/gits/m_bin/*[^.git.] ~/bin
+ln -s ~gits/my_templates/ ~/.vim/
+ln -s ~/gits/dot_files/pomoc.md ~/.vim/
+ln -s ~/gits/m_bin/*[^.git.] ~/bin
